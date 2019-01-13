0c6864993a70e86c974ea1d5798f34ed9e038e36
Lisifan <sf15710696801@163.com>
2019-01-13 18:55:27 +0800

    add weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="title">
+      热销推荐
+    </div>
+    <ul>
+      <li class="item border-bottom"
+          v-for="item of recommendList"
+          :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeWeekend',
+    data () {
+      return {
+        recommendList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1812/b4/e9d7f043c4fadf.jpg_r_640x214_713df0cf.jpg',
+            title:'大连圣亚海洋世界',
+            desc:'浪漫大连首站，浪漫的海洋主题乐园'
+          },
+          {
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1812/b4/e9d7f043c4fadf.jpg_r_640x214_713df0cf.jpg',
+            title:'大连圣亚海洋世界',
+            desc:'浪漫大连首站，浪漫的海洋主题乐园'
+          },
+          {
+            id:'0003',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1812/b4/e9d7f043c4fadf.jpg_r_640x214_713df0cf.jpg',
+            title:'大连圣亚海洋世界',
+            desc:'浪漫大连首站，浪漫的海洋主题乐园'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height:.8rem
+    margin-top: .2rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 33.9%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+  
+
+</style>
